47ea66b92540e0def40f36d34ad791acc6562c4f
Dbuck4life <mrdarrenbuckner@gmail.com>
2025-06-28 12:07:00 -0500

    Successful build: fixed tsconfig and alias issues

--- next.config.js
+++ next.config.js
@@ -3,11 +3,14 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   webpack: (config) => {
-    config.resolve.alias['@components'] = path.resolve(__dirname, 'src/components');
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'src'),
+    };
     return config;
   },
 };
 
 module.exports = nextConfig;
+
