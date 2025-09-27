1e6ed7fc41e0ddde7a64463a7ffe900a7c20268f
DarrenLBuckner <mrdarrenbuckner@gmail.com>
2025-09-26 20:35:22 -0500

    Remove admin/backend files -frontend only

--- src/lib/supabase/backendClient.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const backendSupabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_BACKEND_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_BACKEND_ANON_KEY!
-);
-
-export default backendSupabase;
